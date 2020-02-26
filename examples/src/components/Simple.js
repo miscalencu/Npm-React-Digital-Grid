b28b6d2ad8cdc337c577749b3bea81c4a6d913ed
Ghimisi Cristian <ghimisi_cristi@yahoo.com>
2020-02-26 21:20:32 +0100

    Fixes small bugs

--- examples/src/components/Simple.js
+++ examples/src/components/Simple.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Grid, Column } from 'react-digital-grid';
 
-export default class Simple extends Component {
+const Simple = () => {
+  const gridData = {
+    dataItems: [
+      { Id: 1, FirstName: 'George', LastName: 'Miscalencu' },
+      { Id: 2, FirstName: 'John', LastName: 'Doe' }
+    ]
+  };
 
-    render() {
-
-        let gridData = {
-            dataItems: [
-                { Id: 1, FirstName: 'George', LastName: 'Miscalencu' },
-                { Id: 2, FirstName: 'John', LastName: 'Doe' },
-            ]
-        }
-
-        return (
-            <Grid 
-                gridData={gridData}>
-                    <Column header="Id" dataField="Id"></Column>
-                    <Column header="First Name" dataField="FirstName"></Column>
-                    <Column header="Last Name" dataField="LastName"></Column>
-            </Grid>
-        );
-    };
+  return (
+    <Grid data={gridData.dataItems}>
+      <Column header='Id' dataField='Id'></Column>
+      <Column header='First Name' dataField='FirstName'></Column>
+      <Column header='Last Name' dataField='LastName'></Column>
+    </Grid>
+  );
 };
+
+export default Simple;
